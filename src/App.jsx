435e18ad57068f6ae6bb39722942d3b8cb54860c
Michael-Hills <mjhills1@btinternet.com>
2025-04-15 22:29:52 +0100

    simple pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import { Routes, Route } from 'react-router-dom';
 import Sidebar from './components/sidebar';
+import MapView from './pages/MapView'
+import Profile from './pages/Profile'
+import Visited from './pages/Visited'
 
 function App() {
   return (
@@ -7,8 +10,10 @@ function App() {
       <Sidebar />
       <main className="flex-1 ml-64 p-6">
         <Routes>
-          {/* Add your routes here */}
-          <Route path="/" element={<div>Map View</div>} />
+
+          <Route path="/" element={<MapView/>} />
+          <Route path="/profile" element={<Profile/>} />
+          <Route path="/visited" element={<Visited/>} />
          
         </Routes>
       </main>
---
+++ src/pages/MapView.jsx
@@ -0,0 +1,5 @@
+export default function MapView() {
+  return (
+    <div>Map View Page</div>
+  )
+}
